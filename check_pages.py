648ec895336acdefe2316d4c46b4637e28f78b8a
JialeZhouxin <JialeZhouxin@users.noreply.github.com>
2026-06-29 19:37:46 +0800

    chore: clean up temp files

--- check_pages.py
+++
@@ -1,8 +0,0 @@
-import json, sys
-d = json.load(sys.stdin)
-print('Status:', d.get('status'))
-print('URL:', d.get('html_url'))
-if d.get('status') is None:
-    print('Pages is being deployed... check again in a few minutes.')
-else:
-    print(f'Pages status: {d["status"]}')
